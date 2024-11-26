cf864301d48ea439809857a3c6ce354329de6818
Jordan Glanfield <jordan.glanfield@getmayday.com>
2024-11-26 12:18:56 +0000

    add student-POST and findByEmail repository method

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository // Data access layer
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    Optional<Student> findStudentByEmail(String email);
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail2(String email);
 }
